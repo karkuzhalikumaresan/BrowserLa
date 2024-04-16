9a2b061bc5ce370a782e2b7b146209f566671592
karkuzhalikumaresan <karkuzhalikumaresan@gmail.com>
2024-04-16 18:03:55 +0530

    Broswer changed

--- src/test/Alertsam.java
+++ src/test/Alertsam.java
@@ -4,10 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Alertsam {
 	public static void main(String[] args) {
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new EdgeDriver();
 		driver.get("https://netbanking.hdfcbank.com/netbanking/?_ga=2.176378149.1819882415.1533883212-608727520.1532670997");
 		driver.manage().window().maximize();
 		WebElement element = driver.findElement(By.xpath("//a[@class='btn btn-primary login-btn']"));
